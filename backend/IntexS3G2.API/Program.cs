fb5ff314512fe53ffc24e37349e8c38912f42077
danerjenkins <danej7@byu.edu>
2025-04-07 13:37:00 -0600

    redeploy backend

--- backend/IntexS3G2.API/Program.cs
+++ backend/IntexS3G2.API/Program.cs
@@ -48,7 +48,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000", "https://zealous-water-07b22b61e.6.azurestaticapps.net") // Replace with your frontend URL
+            policy.WithOrigins("http://localhost:3000", "https://zealous-water-07b22b61e.6.azurestaticapps.net","yourmomshouse.com") // Replace with your frontend URL
                 .AllowCredentials() // Required to allow cookies
                 .AllowAnyMethod()
                 .AllowAnyHeader();
