c4d94afe0a9588aa8546d64f277562600ded1fb2
danerjenkins <danej7@byu.edu>
2025-04-06 23:55:25 -0600

    to redeploy

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -87,7 +87,7 @@ function LoginPage() {
                   value={email}
                   onChange={handleChange}
                 />
-                <label htmlFor="email">Email address</label>
+                <label htmlFor="email">Email address.</label>
               </div>
               <div className="form-floating mb-3">
                 <input
