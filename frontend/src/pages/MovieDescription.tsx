fdfd267a37187aec7345af48f3b6d523f6d568af
danerjenkins <danej7@byu.edu>
2025-04-10 21:55:47 -0600

    fix movie description

--- frontend/src/pages/MovieDescription.tsx
+++ frontend/src/pages/MovieDescription.tsx
@@ -40,8 +40,8 @@ export default function MovieDescription() {
   const normalized = movie ? movie.title.normalize('NFD') : '';
   const cleaned = normalized.replace(/[:!%.'--()&#’]/g, '');
   const posterUrl = `${imgUrl}${encodeURIComponent(cleaned)}.jpg`;
-  console.log('movie:', movie);
-  console.log('contentRecs:', contentRecs);
+  // console.log('movie:', movie);
+  // console.log('contentRecs:', contentRecs);
   return (
     <div className="d-flex flex-column min-vh-100">
       <Header />
@@ -64,15 +64,15 @@ export default function MovieDescription() {
                   Back to Movies
                 </button>
               </div>
-                <div className="descriptionBox shadow-lg p-3 mt-3">
-                  <div className="row g-4">
-                    {/* Poster */}
-                    <div className="col-md-3">
-                      <img
-                        src={posterUrl}
-                        alt={`${movie.title} poster`}
-                        onError={(e) => {
-                          const target = e.target as HTMLImageElement;
+              <div className="descriptionBox shadow-lg p-3 mt-3">
+                <div className="row g-4">
+                  {/* Poster */}
+                  <div className="col-md-3">
+                    <img
+                      src={posterUrl}
+                      alt={`${movie.title} poster`}
+                      onError={(e) => {
+                        const target = e.target as HTMLImageElement;
 
                         // Prevent infinite loop if the fallback image also fails
                         if (!target.src.includes('/defaultposter.png')) {
@@ -126,12 +126,14 @@ export default function MovieDescription() {
                   </div>
                 </div>
               </div>
-              <div className="d-flex flex-column mt-5">
-                <MovieList
-                  recommender={`Similar media to ${movie.title}`}
-                  movies={contentRecs}
-                />
-              </div>
+              {contentRecs && (
+                <div className="d-flex flex-column mt-5">
+                  <MovieList
+                    recommender={`Similar media to ${movie.title}`}
+                    movies={contentRecs}
+                  />
+                </div>
+              )}
             </>
           )
         )}
